104a6fac1477e1ecec3fdeb3a618cf3cd8637d07
abdosedroho <abdosedroho@gmail.com>
2024-04-26 06:16:38 +0300

    Add Employee Controller & Employee View Model

---
+++ Employee Addition Form/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Demo.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Addition_Form.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAll();
+            return View();
+        }
+    }
+}
---
+++ Employee Addition Form/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Addition_Form.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required")]
+        [MaxLength(50, ErrorMessage = "Max Length Is 50")]
+        [MinLength(5, ErrorMessage = "Min Length Is 5")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select a job role.")]
+        public string JobRole { get; set; }
+
+        [Required(ErrorMessage = "Please select gender.")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please indicate if this is the first appointment.")]
+        public bool IsFirstAppointment { get; set; }
+
+        [Required(ErrorMessage = "Please select a start date.")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [StringLength(500, ErrorMessage = "Notes cannot exceed 500 characters.")]
+        public string Notes { get; set; }
+    }
+}
